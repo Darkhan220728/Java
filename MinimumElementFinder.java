d32dc6c5689fd9d90d86ff2e58a80e61069072bb
Darkhan Saparbek <220728@astanait.edu.kz>
2023-08-09 03:26:31 +0600

    I've made the code which identifies the minimum element of the array, where  I've added the findMinimumRecursive method which takes the array and the current index as parameters. The base case is when the index reaches the last element of the array, in which case that element is returned as the minimum. Otherwise, the function recursively calculates the minimum of the rest of the array and compares it with the current element at the given index. The smaller value is then returned.

---
+++ MinimumElementFinder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MinimumElementFinder {
+    public static int findMinimumRecursive(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return arr[index];
+        }
+
+        int minRest = findMinimumRecursive(arr, index + 1);
+        return Math.min(arr[index], minRest);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        int minimum = findMinimumRecursive(numbers, 0);
+        System.out.println("The minimum element in the array is: " + minimum);
+    }
+}
