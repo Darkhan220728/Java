ae91e791573be43b2f336e357304bb9f48a288c0
Darkhan Saparbek <220728@astanait.edu.kz>
2023-08-09 03:35:01 +0600

    I've made the code which identifies the minimum element of the array, where  I've added the findMinimumRecursive method which takes the array and the current index as parameters. The base case is when the index reaches the last element of the array, in which case that element is returned as the minimum. Otherwise, the function recursively calculates the minimum of the rest of the array and compares it with the current element at the given index. The smaller value is then returned.

---
+++ AverageCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class AverageCalculator {
+    public static double calculateAverage(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Input array is null or empty.");
+        }
+
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        double average = calculateAverage(numbers);
+        System.out.println("The average of the array is: " + average);
+    }
+}
+
