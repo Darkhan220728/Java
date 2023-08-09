67e31840f429a360c344cb4aab1279f0c9ca9b0f
Darkhan Saparbek <220728@astanait.edu.kz>
2023-08-09 08:18:10 +0600

    I've made the code which identifies the minimum element of the array,… … where I've added the findMinimumRecursive method which takes the array and the current index as parameters. The base case is when the index reaches the last element of the array, in which case that element is returned as the minimum. Otherwise, the function recursively calculates the minimum of the rest of the array and compares it with the current element at the given index. The smaller value is then returned.
    In the calculateAverage function, I iterate through the array and calculate the sum of all elements. Then, we divide the sum by the length of the array to get the average. Note that we cast the sum to a double to ensure that the division yields a floating-point result.
    
    In the main method, I've provided a sample array called numbers to demonstrate how the calculateAverage function can be used. It will print the average of the given array.

--- AverageCalculator.java
+++ AverageCalculator.java
@@ -16,7 +16,6 @@ public class AverageCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
 
